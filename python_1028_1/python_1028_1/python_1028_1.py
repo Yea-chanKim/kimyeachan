cfc807ebf0c5edfd6a6db4098719a20ddbe2fcaf
Yea-chanKim <yeachan10@konkuk.ac.kr>
2015-10-28 14:47:03 +0900

    Update python_1028_1.py

--- python_1028_1/python_1028_1/python_1028_1.py
+++ python_1028_1/python_1028_1/python_1028_1.py
@@ -8,6 +8,8 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen
 from urllib.parse import urljoin
 
+## html 파싱 예제 
+
 #html_doc= """
 #<html><head><title>The Dormouse's story</title></head>
 #<body>
@@ -20,20 +22,21 @@ from urllib.parse import urljoin
 #<p class="story">...</p>
 #"""
 
-#soup = BeautifulSoup(html_doc, "html.parser") # 뒤 인자는 파서종류, 디폴트가 html parser
+#soup = BeautifulSoup(html_doc, "html.parser") # 두번째 인자는 파서종류, 디폴트가 html parser
 ##print(soup.prettify())
 ##print(soup.title.prettify()) # 태그명을 지정해서 찾을 수 있다.
 ##print(soup.title.string) # 태그명의 아이템 값을 얻을 수 있다.
 ##print(soup.p['class']) # 태그의 애트리뷰트 값도 가져올수 있다.(가장 먼저 나오는 값을 가져옴)
 ##print(soup('a',{'id':'link1'})[0].contents) #태그의 애트리뷰트 값을 알면 해당 값을 가져올 수 있다.
-#print(type(soup.find_all(id='link1'))) #리스트가 아닌 bs4의 result set이다!.
+#print(type(soup.find_all(id='link1'))) #리스트가 아닌 bs4의 result set이다.
 
+## 웹사이트 파싱 예제 
 #import webbrowser
 #url = "http://comic.naver.com/webtoon/list.nhn?titleId=20853"
 
 #data = urlopen(url)
 #soup = BeautifulSoup(data,'html.parser')
-#print(soup.title.string)
+#print(soup.title.string) # title 태그의 item을 가져옴
 ##cartoons = soup.find_all('td', {'class' : 'title'}) #td 로 되어있는 태그의 class가 title인 것들을 가져온다.
 
 ##for i in range(len(cartoons)):
@@ -43,7 +46,7 @@ from urllib.parse import urljoin
 ##    print(title, "\t" , tempurl)
 ##    #webbrowser.open_new(tempurl)
 
-##웹 크롤러 만들기
+##웹 크롤러 만들기 예제
 
 class crawler:
     def crawl(self, pages, depth=2):
