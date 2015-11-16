14a315f51e4725b0dc48bda4492fed6a0bca30e2
Yea-chanKim <yeachan10@konkuk.ac.kr>
2015-11-16 13:49:51 +0900

    1116
    1116

---
+++ python_1116_1/python_1116_1/python_1116_1.py
@@ -0,0 +1,16 @@
+﻿from flask import Flask, url_for,redirect
+
+app = Flask(__name__)
+
+@app.route('/hello/<user>')
+def hello_world(user):
+    return redirect(url_for('login'))
+
+@app.route('/login')
+def login():
+    return "Login"
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host = '203.252.166.51',port = 5000)
